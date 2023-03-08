2b87884c1351266b73cc12cb7984d1b077565fbc
Taras <deercompany1122@gmail.com>
2023-03-08 20:55:59 +0200

    add logs

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import discord
+import discord, time
 from discord.ext import commands
 from config import config
 from classes.music import MUSIC
@@ -9,62 +9,73 @@ from classes.clear import CLEAR
 
 bot = commands.Bot(command_prefix=config['prefix'], intents=discord.Intents.all())
 
+f = open('logs.txt', 'a+', encoding='utf-8')
+time1 = time.localtime()
+time = time.strftime("%m/%d/%Y, %H:%M:%S", time1)
 
-@bot.event
-async def on_ready():
-    print("Bot online!")
-
-
-#Play
-@bot.command()
-async def play(ctx, *, command = None):
-    print(ctx, command)
-    await MUSIC().play(ctx, command, bot)
-
-@bot.command()
-async def leave(ctx):
-    await MUSIC().leave(ctx, bot)
-
-@bot.command()
-async def pause(ctx):
-    await MUSIC().pause(ctx, bot)
-
-@bot.command()
-async def resume(ctx):
-    await MUSIC().resume(ctx, bot)
-
-@bot.command()
-async def stop(ctx):
-    await MUSIC().stop(bot)
-
-
-#Filter
 @bot.event
 async def on_message(message):
-   await FILTERS().filters(message, bot)
+   
+    f.write(f'{time}   {message.author} в {message.channel}: {message.content}\n') 
+
+    @bot.event
+    async def on_ready():
+        print("Bot online!")
+
+    
+
+    #Play
+    @bot.command()
+    async def play(ctx, *, command = None):
+        print(ctx, command)
+        await MUSIC().play(ctx, command, bot)
+
+    @bot.command()
+    async def leave(ctx):
+        await MUSIC().leave(ctx, bot)
+
+    @bot.command()
+    async def pause(ctx):
+        await MUSIC().pause(ctx, bot)
+
+    @bot.command()
+    async def resume(ctx):
+        await MUSIC().resume(ctx, bot)
+
+    @bot.command()
+    async def stop(ctx):
+        await MUSIC().stop(bot)
 
 
-#Clear chat
-@bot.command()
-#@commands.has_any_role('BomBitOs', 'King')
-async def clear_all(ctx):
-    await CLEAR().clear_all(ctx)
+    #Filter
+    @bot.event
+    async def on_message(message):
+        await FILTERS().filters(message, bot)
 
 
-@bot.command()
-#@commands.has_any_role('BomBitOs', 'King')
-async def clear(ctx, command = None):
-    await CLEAR().clear(ctx, command)
+    #Clear chat
+    @bot.command()
+    #@commands.has_any_role('BomBitOs', 'King')
+    async def clear_all(ctx):
+        await CLEAR().clear_all(ctx)
 
 
-#Text to Voice message
-@bot.command()
-async def voice(ctx, *, command = None):
-    await VOICE().main(ctx, bot, command)
+    @bot.command()
+    #@commands.has_any_role('BomBitOs', 'King')
+    async def clear(ctx, command = None):
+        await CLEAR().clear(ctx, command)
 
-#Game
-@bot.command()
-async def ssp(ctx):
-    await SSP().main(ctx)
+
+    #Text to Voice message
+    @bot.command()
+    async def voice(ctx, *, command = None):
+        await VOICE().main(ctx, bot, command)
+
+    #Game
+    @bot.command()
+    async def ssp(ctx):
+        await SSP().main(ctx)
+
+f.close
 
 bot.run(config['token'])
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 config = {
     "prefix": "!",
-    "token": "MTAyODMzNTgyNTU1MDg0ODEyMA.GmiEdP.QDpUwyvxUKn9QJmXmUnRYG4YQj8KT_W7Bs7o9A",
+    "token": "MTAyODMzNTgyNTU1MDg0ODEyMA.GhPVko.G1jadLs0ULpgSCUBQttmUQu04eOAbtRJgI4LC8",
 }
